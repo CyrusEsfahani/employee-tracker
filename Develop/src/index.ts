9ca4e2b531365234f8b5f8dd30bc5bc81edd0835
Heisenberg <esfahani.cyrus@gmail.com>
2024-09-03 22:17:12 -0700

    finished up two working functions

--- Develop/src/index.ts
+++ Develop/src/index.ts
@@ -7,50 +7,51 @@ function app(): void {
         name: "action",
         message: "What would you like to do?",
         choices: [
-            "View all departments",
-            "View all roles",
-            "View all employees",
-            "Add a department",
-            "Add a role",
-            "Add an employee",
-            "Update an employee role",
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add A Department",
+            "Add A Role",
+            "Add An Employee",
+            "Update An Employee Role",
             "Quit"
         ]
     }).then(({ action }) => {
         switch (action) {
-            case "view All Employees":
+            case "View All Employees":
                 viewAllEmployees();
                 break;
             case "view All Employees by Department":
-                viewAllEmployeesByDepartment();
+                // viewAllEmployeesByDepartment();
                 break;
             case "view All Employees by Manager":
-                viewAllEmployeesByManager();
+                // viewAllEmployeesByManager();
                 break;
-            case "Add Employee":
+            case "Add An Employee":
                 addEmployee();
                 break;
             case "Remove Employee":
-                removeEmployee();
+                // removeEmployee();
                 break;
             case "Update Employee Role":
-                updateEmployeeRole();
+                // updateEmployeeRole();
                 break;
             case "Update Employee Manager":
-                updateEmployeeManager();
+                // updateEmployeeManager();
                 break;
             case "View All Roles":
                 viewAllRoles();
                 break;
             case "Add Role":
-                addRole();
+                // addRole();
                 break;
             case "view All Departments":
                 viewAllDepartments();
                 break;
             case "Add Department":
-                addDepartment();
+                // addDepartment();
                 break;
+            
 
             case "Quit":
                 
@@ -64,7 +65,7 @@ function app(): void {
 }
 
 async function viewAllEmployees(): Promise<void> {
-    const sql = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager.first_name || ' ' || manager.last_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager_id = employee.manager.id;"
+   const sql = "SELECT employee.id, employee.first_name AS \"first name\", employee.last_name AS \"last name\", role.title, department.name AS department, role.salary, manager.first_name || ' ' || manager.last_name AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;"
 
     const employees = await pool.query(sql);
     console.table(employees.rows);
@@ -88,8 +89,8 @@ async function viewAllRoles(): Promise<void> {
 async function addEmployee(): Promise<void> {
    const roles = await pool.query("SELECT id as value, title as name FROM role");
    const employees = await pool.query("SELECT id as value, first_name || ' ' || last_name as name FROM employee");
-   inquirer.prompt([])
-}
+   inquirer.prompt([
+
 {
     type: "input",
     name: "first_name",
@@ -112,3 +113,11 @@ async function addEmployee(): Promise<void> {
     message: "Select employee's manager",
     choices: employees.rows
 }
+   ]).then((answers) => {
+       const sql = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)";
+       pool.query(sql, [answers.first_name, answers.last_name, answers.role_id, answers.manager_id]);
+       app();
+   })
+}
+
+app();
